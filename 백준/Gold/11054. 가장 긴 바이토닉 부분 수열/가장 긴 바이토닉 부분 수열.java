d95486dbbef688f2747a74e179112a2555a56afe
pedada <korea9300@naver.com>
2024-08-06 20:16:01 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 112 ms, Memory: 12312 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[] nums = new int[N];
+
+        int[] dp1 = new int[N];
+        Arrays.fill(dp1, 1);
+        int[] dp2 = new int[N];
+        Arrays.fill(dp2, 1);
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]){
+                    dp1[i] = Math.max(dp1[i], dp1[j]+1);
+                }
+            }
+        }
+        for (int i = N-1; i >= 0; i--) {
+            for (int j = N-1; j > i; j--) {
+                if (nums[j] < nums[i]){
+                    dp2[i] = Math.max(dp2[i], dp2[j]+1);
+                }
+            }
+        }
+        int ans = 1;
+        for (int i = 0; i < N; i++) {
+            ans = Math.max(ans, dp1[i] + dp2[i]-1);
+        }
+        System.out.println(ans);
+    }
+}
