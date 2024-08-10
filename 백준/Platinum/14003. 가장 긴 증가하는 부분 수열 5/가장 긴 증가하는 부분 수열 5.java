5635a170763a942dc238e9f80d54509b87209a09
pedada <korea9300@naver.com>
2024-08-11 01:51:06 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 780 ms, Memory: 395004 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder ans;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[] nums = new int[N];
+        ArrayList<Node> dp = new ArrayList<>();
+        ans = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        search: for (int input : nums) {
+            if (dp.isEmpty()){
+                dp.add(new Node(null, input));
+            }else{
+                if (dp.get(dp.size()-1).value < input){
+                    dp.add(new Node(dp.get(dp.size()-1), input));
+                }
+                int start = 0;
+                int end = dp.size();
+                while (start != end){
+                    int mid = (start + end)/2;
+                    if (dp.get(mid).value < input){
+                        start = mid + 1;
+                    }else{
+                        end = mid;
+                    }
+                }
+                if (dp.get(end).value != input){
+                    if (end == 0){
+                        dp.set(end, new Node(null, input));
+                    }else{
+                        dp.set(end, new Node(dp.get(end-1), input));
+                    }
+                }
+            }
+        }
+        System.out.println(dp.size());
+        Node node = dp.get(dp.size() - 1);
+        dfs(node);
+        System.out.println(ans.toString().trim());
+    }
+
+    private static void dfs(Node node){
+        if (node.parent != null){
+            dfs(node.parent);
+        }
+        ans.append(node.value + " ");
+    }
+
+    public static class Node{
+        Node parent;
+        int value;
+
+        public Node(Node parent, int value) {
+            this.parent = parent;
+            this.value = value;
+        }
+    }
+}
