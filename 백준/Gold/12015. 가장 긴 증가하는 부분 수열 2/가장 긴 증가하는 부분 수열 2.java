4863c0457ec35326eb3491cc06282608c8b9ef54
pedada <korea9300@naver.com>
2024-08-11 01:16:02 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 532 ms, Memory: 149288 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -6,34 +6,32 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine().trim());
-        int[] A = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[] nums = new int[N];
+        ArrayList<Integer> dp = new ArrayList<>();
 
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            nums[i] = Integer.parseInt(st.nextToken());
         }
-
-        List<Integer> dp = new ArrayList<>();
-
-        for (int input : A) {
-            if (dp.isEmpty() || dp.get(dp.size() - 1) < input) {
+        search: for (int input : nums) {
+            if (dp.isEmpty() || dp.get(dp.size()-1) < input){
                 dp.add(input);
-            } else {
+            }else{
                 int start = 0;
                 int end = dp.size();
-                while (start < end) {
-                    int mid = (start + end) / 2;
-                    if (dp.get(mid) < input) {
+                while (start != end){
+                    int mid = (start + end)/2;
+                    if (dp.get(mid) < input){
                         start = mid + 1;
-                    } else {
+                    }else{
                         end = mid;
                     }
                 }
                 dp.set(end, input);
             }
         }
-
         System.out.println(dp.size());
     }
 }
