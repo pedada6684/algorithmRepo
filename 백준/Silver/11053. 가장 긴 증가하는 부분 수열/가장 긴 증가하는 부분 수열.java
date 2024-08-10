77759b7284e3720aa46a8828e7e5ac6ee0b780e8
pedada <korea9300@naver.com>
2024-08-11 00:51:52 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 76 ms, Memory: 12344 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,32 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int[] nums = new int[N];
-        int[] dp = new int[N];
-        Arrays.fill(dp, 1);
+        LinkedList<Integer> dp = new LinkedList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int ans = 1;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]){
-                    dp[i] = Math.max(dp[i], dp[j]+1);
-                    ans = Math.max(ans, dp[i]);
+        int maxx = Integer.MIN_VALUE;
+        input: for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(st.nextToken());
+            if (dp.isEmpty()){
+                dp.add(input);
+                maxx = input;
+            }else{
+                if (maxx < input){
+                    maxx = input;
+                    dp.add(input);
+                    continue input;
+                }
+                int start = 0;
+                int end = dp.size();
+                while (start != end){
+                    int mid = (start + end)/2;
+                    if (dp.get(mid) == input){
+                        continue input;
+                    }else if (dp.get(mid) < input){
+                        start = mid + 1;
+                    }else{
+                        end = mid;
+                    }
+                }
+                dp.remove(end);
+                dp.add(end, input);
+                if (end == dp.size()-1){
+                    maxx = input;
                 }
             }
         }
-        System.out.println(ans);
+//        for (int i = 0; i < dp.size(); i++) {
+//            System.out.print(dp.get(i)+" ");
+//        }
+//        System.out.println();
+        System.out.println(dp.size());
     }
 }
