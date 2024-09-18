8e18a9fd7bad6adc37980f75cc195791627792ab
pedada <korea9300@naver.com>
2024-09-19 02:18:19 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 96 ms, Memory: 21488 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            q.add(i);
+        }
+        sb.append("<");
+        while (!q.isEmpty()){
+            for (int i = 0; i < K-1; i++) {
+                q.add(q.poll());
+            }
+            sb.append(q.poll());
+            if (!q.isEmpty()){
+                sb.append(", ");
+            }
+        }
+        sb.append(">");
+        System.out.println(sb);
+
+    }
+}
