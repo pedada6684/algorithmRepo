c669c0e756ccc528455a8a7b92ba0ce83ea257d1
pedada <korea9300@naver.com>
2024-09-30 01:09:08 +0900

    [Silver II] Title: 최대 힙, Time: 396 ms, Memory: 37688 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+        StringBuilder sb  = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            if (x == 0){
+                if (pq.isEmpty()){
+                    sb.append("0\n");
+                }else{
+                    sb.append(pq.poll()+"\n");
+                }
+            }else{
+                pq.add(x);
+            }
+        }
+        System.out.println(sb.toString().trim());
+    }
+}
