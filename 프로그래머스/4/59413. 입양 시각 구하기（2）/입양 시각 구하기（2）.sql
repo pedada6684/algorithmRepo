a3eb9468ba1f49cf857648eb6ce18b145baf0bcd
pedada <korea9300@naver.com>
2024-10-12 16:20:23 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,11 +1,7 @@
-SET @hour := -1;
+set @hour := -1;
 
-SELECT 
-    (@hour := @hour+1) AS HOUR,
-    (
-        select count(*) from ANIMAL_OUTS where hour(DATETIME) = @hour
-    ) as COUNT
-FROM 
-    ANIMAL_OUTS 
-Where
-    @hour < 23;
+select 
+    (@hour := @hour +1) as HOUR,
+    (select count(*) from ANIMAL_OUTS where hour(DATETIME) = @hour) as COUNT
+from ANIMAL_OUTS
+where @hour<23;
