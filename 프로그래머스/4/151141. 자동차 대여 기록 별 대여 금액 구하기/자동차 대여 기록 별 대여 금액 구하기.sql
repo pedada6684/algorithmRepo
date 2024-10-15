1da11d5f6aa293fbefde8f4ddf142acccf8b8220
pedada <korea9300@naver.com>
2024-10-16 01:24:59 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,37 +1,40 @@
--- 코드를 입력하세요
-SELECT h.HISTORY_ID, 
-# (datediff(h.END_DATE, h.START_DATE)+1) * c.DAILY_FEE as OFee,
-case
-    when (datediff(h.END_DATE, h.START_DATE)+1) < 7 
-        then (datediff(h.END_DATE, h.START_DATE)+1) * c.DAILY_FEE
-    when 7 <= (datediff(h.END_DATE, h.START_DATE)+1) and (datediff(h.END_DATE, h.START_DATE)+1) < 30
-        then round((datediff(h.END_DATE, h.START_DATE)+1) * c.DAILY_FEE * 
-            (1 - (
-                select DISCOUNT_RATE 
-                from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-                where CAR_TYPE = c.CAR_TYPE
-                and DURATION_TYPE like '7%'
-             )*0.01))
-     when 30 <= (datediff(h.END_DATE, h.START_DATE)+1) and (datediff(h.END_DATE, h.START_DATE)+1) < 90
-        then round((datediff(h.END_DATE, h.START_DATE)+1) * c.DAILY_FEE * 
-            (1 - (
-                select DISCOUNT_RATE 
-                from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-                where CAR_TYPE = c.CAR_TYPE
-                and DURATION_TYPE like '30%'
-             )*0.01))
-    when 90 <= (datediff(h.END_DATE, h.START_DATE)+1)
-        then round((datediff(h.END_DATE, h.START_DATE)+1) * c.DAILY_FEE * 
-            (1 - (
-                select DISCOUNT_RATE 
-                from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-                where CAR_TYPE = c.CAR_TYPE
-                and DURATION_TYPE like '90%'
-             )*0.01))
-end as fee
-from CAR_RENTAL_COMPANY_CAR c
-join CAR_RENTAL_COMPANY_RENTAL_HISTORY h
-    on c.CAR_ID = h.CAR_ID
-where CAR_TYPE = '트럭'
-order by FEE desc, HISTORY_ID desc
-# # select * from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+WITH RentalDurations AS (
+    SELECT 
+        h.HISTORY_ID, 
+        h.CAR_ID,
+        DATEDIFF(h.END_DATE, h.START_DATE) AS Rental_Duration
+    FROM 
+        CAR_RENTAL_COMPANY_RENTAL_HISTORY h
+)
+
+SELECT 
+    rd.HISTORY_ID, 
+    ROUND((rd.Rental_Duration + 1) * c.DAILY_FEE * 
+        CASE 
+            WHEN rd.Rental_Duration + 1 < 7 THEN 1
+            WHEN rd.Rental_Duration + 1 < 30 THEN 
+                (1 - (SELECT DISCOUNT_RATE / 100 
+                      FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
+                      WHERE CAR_TYPE = c.CAR_TYPE 
+                      AND DURATION_TYPE LIKE '7%'))
+            WHEN rd.Rental_Duration + 1 < 90 THEN 
+                (1 - (SELECT DISCOUNT_RATE / 100 
+                      FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
+                      WHERE CAR_TYPE = c.CAR_TYPE 
+                      AND DURATION_TYPE LIKE '30%'))
+            ELSE 
+                (1 - (SELECT DISCOUNT_RATE / 100 
+                      FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
+                      WHERE CAR_TYPE = c.CAR_TYPE 
+                      AND DURATION_TYPE LIKE '90%'))
+        END
+    ) AS fee
+FROM 
+    RentalDurations rd
+JOIN 
+    CAR_RENTAL_COMPANY_CAR c ON c.CAR_ID = rd.CAR_ID
+WHERE 
+    c.CAR_TYPE = '트럭'
+ORDER BY 
+    fee DESC, 
+    rd.HISTORY_ID DESC;
