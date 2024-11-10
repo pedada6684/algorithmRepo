1db750f9fed75115b75e7cd4f491df47e501f4f8
pedada <korea9300@naver.com>
2024-11-11 00:41:33 +0900

    [Silver III] Title: N과 M (7), Time: 612 ms, Memory: 294660 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] sel;
+    static int[] arr;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        sb = new StringBuilder();
+        sel = new int[M];
+        comb(0);
+        System.out.println(sb.toString().trim());
+    }
+
+    private static void comb(int sidx) {
+        if (sidx == sel.length){
+            for (int i : sel) {
+                sb.append(i+" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            sel[sidx] = arr[i];
+            comb(sidx+1);
+        }
+    }
+}
