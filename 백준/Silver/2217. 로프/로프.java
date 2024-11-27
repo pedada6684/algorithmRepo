33130f7c66d2d702342f7a267969c4fa97b69875
pedada <korea9300@naver.com>
2024-11-28 01:54:53 +0900

    [Silver IV] Title: 로프, Time: 248 ms, Memory: 25740 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            ans = Math.max(ans, arr[i]*(N-i));
+        }
+        System.out.println(ans);
+    }
+}
