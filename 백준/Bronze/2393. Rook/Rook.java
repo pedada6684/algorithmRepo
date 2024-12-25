c768ff180e9832f8be2448d5c7d69c5abf9f7e22
pedada <korea9300@naver.com>
2024-12-26 01:37:20 +0900

    [Bronze V] Title: Rook, Time: 100 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("""
+                  ___  ___  ___
+                  | |__| |__| |
+                  |           |
+                   \\_________/
+                    \\_______/
+                     |     |
+                     |     |
+                     |     |
+                     |     |
+                     |_____|
+                  __/       \\__
+                 /             \\
+                /_______________\\""");
+    }
+}
