603f06ccb4b609aa76ca42305d996f1db0fe387e
pedada <korea9300@naver.com>
2025-01-08 00:44:19 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1672 ms, Memory: 314772 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -4,8 +4,8 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr, parent;
-    static int[] v;
+    static int[] next, parent;
+    static short[] v;
     static boolean flag;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,64 +15,52 @@ public class Main {
         for (int tc = 0; tc < TC; tc++) {
             st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
-            arr = new int[N];
-            v = new int[N];
+            next = new int[N];
+            v = new short[N];
             parent = new int[N];
             for (int i = 0; i < parent.length; i++) {
                 parent[i] = i;
             }
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = Integer.parseInt(st.nextToken())-1;
+            for (int i = 0; i < next.length; i++) {
+                next[i] = Integer.parseInt(st.nextToken())-1;
             }
-            for (int i = 0; i < arr.length; i++) {
-                if (v[i] == 1) continue;
+            for (int i = 0; i < next.length; i++) {
+                if (v[i] != 0) continue;
                 flag = false;
-                dfs(i, arr[i]);
+                dfs(i, next[i]);
             }
             int cnt = 0;
             for (int i : v) {
-//                System.out.print(i+" ");
                 if (i != 1) cnt++;
             }
-//            System.out.println();
             ans.append(cnt+"\n");
-
-
         }
         System.out.println(ans.toString().trim());
     }
 
     private static void dfs(int from, int to) {
-//        System.out.println("from : "+from +" to: "+to);
         if (v[to] != 0){
-//            System.out.println("fail: "+from);
             v[from] = -1;
             flag = true;
             return;
         }
-//        System.out.println("==from : "+from +" to: "+to);
         if (parent[from] == parent[to]){
-//            System.out.println("makeGroup "+to);
             makeGroup(to);
             flag = true;
             return;
         }
         parent[to] = parent[from];
-        dfs(to, arr[to]);
+        dfs(to, next[to]);
         if (flag && v[from] != 1) {
-//            System.out.println("fail: "+from);
             v[from] = -1;
         }
     }
 
-    private static void makeGroup(int to) {
-//        System.out.println("start: "+to);
-        while (v[to] != 1){
-            v[to] = 1;
-            to = arr[to];
-//            System.out.println("next: "+to);
+    private static void makeGroup(int now) {
+        while (v[now] != 1){
+            v[now] = 1;
+            now = next[now];
         }
-//        System.out.println("==========");
     }
 }
