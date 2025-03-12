8a3a37a7a0ee9a16ec50dbb6e2364dd55c80e409
pedada <korea9300@naver.com>
2025-03-13 01:20:28 +0900

    [Silver IV] Title: 오리와 박수치는 춘배, Time: 224 ms, Memory: 26476 KB -BaekjoonHub

---
+++ 백준/Silver/30404. 오리와 박수치는 춘배/오리와 박수치는 춘배.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int cnt = 0;
+        int clap = 0;
+        for (int qq : arr) {
+            if (clap >= qq) continue;
+            clap = qq+K;
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
