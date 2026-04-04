697db18b8e48d6c8f6a546eb2ebc85d992addd7a
pedada <korea9300@naver.com>
2026-04-04 17:32:00 +0900

    [Gold III] Title: 내리막 길, Time: 280 ms, Memory: 37380 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,22 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N,M;
+
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
     static int[][] map;
     static int[][] memo;
-    static int[] dx = {-1,0,1,0};
-    static int[] dy = {0,1,0,-1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
         map = new int[N][M];
         memo = new int[N][M];
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
@@ -24,24 +28,25 @@ public class Main {
                 memo[i][j] = -1;
             }
         }
-        System.out.println(dp(0,0));
+        memo[N-1][M-1] = 1;
+
+        System.out.println(dp(0, 0));
     }
 
-    private static int dp(int i, int j) {
-        if (memo[i][j] != -1){
-            return memo[i][j];
-        }if (i == N-1 && j == M-1){
-            return memo[i][j] = 1;
+    static int dp (int x, int y){
+        if(memo[x][y] != -1){
+            return memo[x][y];
         }
+        memo[x][y] = 0;
 
-        memo[i][j] = 0;
-        for (int d = 0; d < 4; d++) {
-            int nx = i+dx[d];
-            int ny = j+dy[d];
-            if (0<= nx && nx < N && 0 <= ny && ny < M && map[nx][ny] < map[i][j]){
-                memo[i][j] += dp(nx, ny);
+        for (int i = 0; i < 4; i++) {
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(0<= nx && nx<map.length && 0 <= ny && ny < map[0].length && map[x][y] > map[nx][ny]){
+                memo[x][y] += dp(nx,ny);
             }
         }
-        return memo[i][j];
+
+        return memo[x][y];
     }
 }
